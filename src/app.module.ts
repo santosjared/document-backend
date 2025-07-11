dd779eea11d3f6c6a54b867fe2c23bfc752993bf
santitos <santosjared221234@gmail.com>
2025-07-11 08:07:20 -0400

    fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,24 @@
-import { Module } from '@nestjs/common';
-import { DelayModule } from './delay/delay.module';
-import { DocumentsModule } from './documents/documents.module';
-import { ConfigModule } from '@nestjs/config';
-import environment from './config/environment';
-import getConfig from './config/environment'
-import { MongooseModule } from '@nestjs/mongoose';
-
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
+import { DelayModule } from './delay/delay.module'
+import { DocumentsModule } from './documents/documents.module'
+import environment from './config/environment'
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal:true,
-    load:[environment]
-    
-  }),
-  MongooseModule.forRoot(getConfig().MONGO_URI), 
-    DelayModule, DocumentsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [environment], // 👍 sigue usando tu archivo environment.ts
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_URI'), // 🔥 ahora se lee desde lo que devuelve environment.ts
+      }),
+    }),
+    DelayModule,
+    DocumentsModule,
+  ],
 })
 export class AppModule {}
